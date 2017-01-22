7650df941cce766d688db47e2367a84576c56466
Nicolai <nicolai@mmg.io>
2017-01-22 12:14:34 +0000

    More node work with status codes and JSON handlers

--- app.js
+++ app.js
@@ -2,13 +2,21 @@
 
 var https = require("https");
 
-var username = "nicolaihowells";
+var username = "nicolaihowells789";
 
 // Solution: Use node.js to connect to Treehouse's API to get profile information to print out
+
+// Print out message
 
 function printMessage(username, badgeCount, points) {
  var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in JavaScript";
  console.log(message);
+}
+
+// Print out error messages
+
+function printError(error) {
+ console.error(error.message);
 }
 
 // Connect to the API url (http://teamtreehouse.com/username.json)
@@ -22,14 +30,25 @@ var request = https.get("https://teamtreehouse.com/" + username + ".json", funct
   body += chunk;
  });
 
- response.on('end', function() {
-  console.log(body);
- });
-
-});
 // Parse the data
 // Print the data
 
-request.on("error", function(error) {
- console.error(error.message);
+ response.on('end', function() {
+  if (response.statusCode === 200) {
+   try {
+   var profile = JSON.parse(body);
+   printMessage(username, profile.badges.length, profile.points.JavaScript)
+   } catch(error) {
+    // Parse Error
+    printError(error);
+   }
+  } else {
+   // Status code error
+   printError({message: "There was an error getting the profile for " + username + ". (" + response.statusCode + ")"});
+  }
+ });
+
 });
+
+// Connection Error
+request.on("error", printError);
